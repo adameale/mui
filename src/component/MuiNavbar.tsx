e67d0c37d37b12d8a86ad79c78b60aed2db4dae3
adameale <aderajewa569@gmail.com>
2023-03-31 04:23:48 -0700

    navbar

--- src/component/MuiNavbar.tsx
+++ src/component/MuiNavbar.tsx
@@ -6,8 +6,8 @@ import { AppBar,Toolbar,Typography,
  import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon'
 const MuiNavbar = () => {
   return (
-    <AppBar position='static'>
-      <Toolbar>
+    <AppBar  position='static'>
+      <Toolbar >
         <IconButton size='large' edge='start' 
         color='inherit' aria-label='logo'>
             < CatchingPokemonIcon/>
@@ -15,6 +15,13 @@ const MuiNavbar = () => {
         </IconButton>
         <Typography variant='h5' component='div'  
         sx={{ flexGrow:1}}>App bar</Typography>
+
+        <Stack  spacing={2} direction='row'>
+            <Button color='inherit' >feature</Button>
+            <Button color='inherit' >about us</Button>
+            <Button color='inherit' >pricing</Button>
+            <Button color='inherit' >login</Button>
+        </Stack>
       </Toolbar>
     </AppBar>
   )
